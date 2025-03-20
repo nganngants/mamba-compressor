3650087b802ce90a14a5f9b663dee167cbc805e5
nganngants <21520069@gm.uit.edu.vn>
2025-03-21 00:43:29 +0700

    add setup script

--- vast_setup.sh
+++ vast_setup.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+touch ~/.no_auto_tmux
+
+conda env create -f environment.yml
+
+conda init
+
+source ~/.bashrc
+
+conda activate smes_multimodal
+
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+
+conda install -c nvidia cuda-compiler
+pip uninstall bitsandbytes -y
+pip install --upgrade bitsandbytes
+pip install deepspeed
+pip install h5py
+pip install flash-attn --no-build-isolation
+pip uninstall deepspeed -y
+pip install deepspeed==0.15.4
