97c3813445d9e2b2c2769c4bbf39dfe16260d585
nganngants <21520069@gm.uit.edu.vn>
2025-03-31 09:11:45 +0700

    add installing VideoLLaMA2

--- install_deps.sh
+++ install_deps.sh
@@ -11,7 +11,14 @@ pip install -e .
 git config --global user.email "nganngants@gmail.com"
 git config --global user.name "nganngants"
 
+gdown --folder https://drive.google.com/drive/folders/17f8lbKdSSN7A3gaZkIvgAVKLnlCNqDI7?usp=drive_link
 gdown --folder https://drive.google.com/drive/folders/1-CYUhPkADMc4I5sycGrT7TP78iTU87wN\?usp\=drive_link
 gdown 17nHhoVx8im-fjsY7cqFL2OrMwVusHMHJ
 
 unzip mamba_ckpt.zip
+
+git submodule update --init
+cd VideoLLaMA2
+git checkout audio_visual
+pip install -e .
+cd ..
