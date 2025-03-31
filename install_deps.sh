8bf718eb836b0a26dcaad1cc40eaea58bc17c386
nganngants <21520069@gm.uit.edu.vn>
2025-03-31 09:08:20 +0700

    update

---
+++ install_deps.sh
@@ -0,0 +1,17 @@
+pip uninstall bitsandbytes -y
+pip install --upgrade bitsandbytes
+pip install deepspeed
+pip install h5py
+pip install flash-attn --no-build-isolation
+pip uninstall deepspeed -y
+pip install deepspeed==0.15.4
+pip install gdown
+pip install -e .
+
+git config --global user.email "nganngants@gmail.com"
+git config --global user.name "nganngants"
+
+gdown --folder https://drive.google.com/drive/folders/1-CYUhPkADMc4I5sycGrT7TP78iTU87wN\?usp\=drive_link
+gdown 17nHhoVx8im-fjsY7cqFL2OrMwVusHMHJ
+
+unzip mamba_ckpt.zip
--- vast_setup.sh
+++ vast_setup.sh
@@ -12,22 +12,3 @@ source ~/.bashrc
 
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 apt-get install unzip -y
-
-conda install -c nvidia cuda-compiler -y
-pip uninstall bitsandbytes -y
-pip install --upgrade bitsandbytes
-pip install deepspeed
-pip install h5py
-pip install flash-attn --no-build-isolation
-pip uninstall deepspeed -y
-pip install deepspeed==0.15.4
-pip install gdown
-pip install -e .
-
-git config --global user.email "nganngants@gmail.com"
-git config --global user.name "nganngants"
-
-gdown --folder https://drive.google.com/drive/folders/1-CYUhPkADMc4I5sycGrT7TP78iTU87wN\?usp\=drive_link
-gdown 17nHhoVx8im-fjsY7cqFL2OrMwVusHMHJ
-
-unzip mamba_ckpt.zip
